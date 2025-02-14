0414616aa67b64e13742cf298ffc43e783e1b065
S Ke <sskset@gmail.com>
2025-02-15 09:08:51 +1100

    Initial Commit

---
+++ FruitMergeTests/FruitMergeTests.swift
@@ -0,0 +1,17 @@
+//
+//  FruitMergeTests.swift
+//  FruitMergeTests
+//
+//  Created by Shan Ke on 15/2/2025.
+//
+
+import Testing
+@testable import FruitMerge
+
+struct FruitMergeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ FruitMergeUITests/FruitMergeUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  FruitMergeUITestsLaunchTests.swift
+//  FruitMergeUITests
+//
+//  Created by Shan Ke on 15/2/2025.
+//
+
+import XCTest
+
+final class FruitMergeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
